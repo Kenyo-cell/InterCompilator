c1d8a701db8f686bebd6142fca247912dffc2626
d0rj <hanaan@internet.ru>
2022-02-12 23:45:11 +0300

    feat: add language select example

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-var editor = monaco.editor.create(document.getElementById('code-container'), {
-    value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-    language: 'javascript',
+var editor = null;
+var languageExamples = {
+    'javascript': "function hi() {\n\tconsole.log('Hello, world!');\n}\n",
+    'python': "def main():\n\tprint('Hello, world!')\n",
+    'scala': '@main def hello = println("Hello, world!")\n'
+}
+
+
+function selectedModel() {
+    var select = document.getElementById('models-select');
+    return select.options[select.selectedIndex].value;
+}
+
+
+function createLanguageModel(modelLanguage) {
+    return monaco.editor.createModel(languageExamples[modelLanguage], modelLanguage);
+}
+
+
+var avaliableModels = (function () {
+    var select = document.getElementById('models-select');
+    var modelsNames = [];
+    for (var i = 0; i < select.childNodes.length; ++i) {
+        modelsNames.push(select.childNodes[i].value);
+    }
+    modelsNames = modelsNames.filter(x => x);
+
+    var avaliableModels = {}
+    for (var i = 0; i < modelsNames.length; ++i) {
+        var model = modelsNames[i];
+        avaliableModels[model] = createLanguageModel(model);
+    }
+    return avaliableModels;
+})();
+
+
+function onEditorTypeChanged(selectedObject) {
+    var type = selectedObject.value;
+    editor.setModel(avaliableModels[type]);
+}
+
+
+editor = monaco.editor.create(document.getElementById('code-container'), {
+    value: languageExamples[selectedModel()],
+    language: selectedModel(),
     scrollBeyondLastLine: false,
 });
