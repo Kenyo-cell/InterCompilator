2c9f589626aefbf83f4a57092f34c3b1ee84e55e
d0rj <hanaan@internet.ru>
2022-02-14 23:34:46 +0300

    feat: hide all objects from user

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
-var editor = null;
-var languageExamples = {
-    'javascript': "function hi() {\n\tconsole.log('Hello, world!');\n}\n",
-    'python': "def main():\n\tprint('Hello, world!')\n",
-    'scala': '@main def hello = println("Hello, world!")\n'
-}
+const [onEditorTypeChanged, runCode] = (function () {
+    var exportObjects = [];
 
-
-function selectedModel() {
-    var select = document.getElementById('models-select');
-    return select.options[select.selectedIndex].value;
-}
-var currentLanguage = selectedModel();
-
-
-function createLanguageModel(modelLanguage) {
-    return monaco.editor.createModel(languageExamples[modelLanguage], modelLanguage);
-}
-
-
-var avaliableModels = (function () {
-    var select = document.getElementById('models-select');
-    var modelsNames = [];
-    for (var i = 0; i < select.childNodes.length; ++i) {
-        modelsNames.push(select.childNodes[i].value);
+    var editor = null;
+    var languageExamples = {
+        'javascript': "function hi() {\n\tconsole.log('Hello, world!');\n}\n",
+        'python': "def main():\n\tprint('Hello, world!')\n",
+        'scala': '@main def hello = println("Hello, world!")\n'
     }
-    modelsNames = modelsNames.filter(x => x);
 
-    var avaliableModels = {}
-    for (var i = 0; i < modelsNames.length; ++i) {
-        var model = modelsNames[i];
-        avaliableModels[model] = createLanguageModel(model);
+
+    function selectedModel() {
+        var select = document.getElementById('models-select');
+        return select.options[select.selectedIndex].value;
     }
-    return avaliableModels;
+    var currentLanguage = selectedModel();
+
+
+    function createLanguageModel(modelLanguage) {
+        return monaco.editor.createModel(languageExamples[modelLanguage], modelLanguage);
+    }
+
+
+    var avaliableModels = (function () {
+        var select = document.getElementById('models-select');
+        var modelsNames = [];
+        for (var i = 0; i < select.childNodes.length; ++i) {
+            modelsNames.push(select.childNodes[i].value);
+        }
+        modelsNames = modelsNames.filter(x => x);
+
+        var avaliableModels = {}
+        for (var i = 0; i < modelsNames.length; ++i) {
+            var model = modelsNames[i];
+            avaliableModels[model] = createLanguageModel(model);
+        }
+        return avaliableModels;
+    })();
+
+
+    function onEditorTypeChanged(selectedObject) {
+        var type = selectedObject.value;
+        currentLanguage = type;
+        editor.setModel(avaliableModels[type]);
+    }
+    exportObjects.push(onEditorTypeChanged);
+
+
+    function runCode() {
+        var code = editor.getValue();
+        var language = currentLanguage;
+        console.log({
+            'language': language,
+            'code': code,
+        });
+    }
+    exportObjects.push(runCode);
+
+
+    editor = monaco.editor.create(document.getElementById('code-container'), {
+        value: languageExamples[selectedModel()],
+        language: selectedModel(),
+
+        scrollBeyondLastLine: false,
+        roundedSelection: false,
+        lineNumbers: 'on',
+        readOnly: false,
+        wordWrap: 'on',
+        theme: 'vs'
+    });
+
+    return exportObjects;
 })();
-
-
-function onEditorTypeChanged(selectedObject) {
-    var type = selectedObject.value;
-    currentLanguage = type;
-    editor.setModel(avaliableModels[type]);
-}
-
-
-function runCode() {
-    var code = editor.getValue();
-    var language = currentLanguage;
-    console.log(code);
-    console.log(language);
-}
-
-
-editor = monaco.editor.create(document.getElementById('code-container'), {
-    value: languageExamples[selectedModel()],
-    language: selectedModel(),
-
-    scrollBeyondLastLine: false,
-    roundedSelection: false,
-    lineNumbers: 'on',
-    readOnly: false,
-    wordWrap: 'on',
-    theme: 'vs'
-});
-console.log(editor);
