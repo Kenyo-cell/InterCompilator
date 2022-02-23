abce169012af6dbafae2a22228efe5a70807e83d
Kenyo-cell <rekurt3000@gmail.com>
2022-02-23 23:02:57 +0300

    feat: add starter multipart processing

--- src/main/java/ru/mirea/controller/MainController.java
+++ src/main/java/ru/mirea/controller/MainController.java
@@ -2,16 +2,21 @@ package ru.mirea.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Controller
 public class MainController {
+    @CrossOrigin
     @PostMapping("/submit")
-    public ResponseEntity<String> sendToExecute(@RequestPart String language, @RequestPart MultipartFile file) {
+    public ResponseEntity<String> sendToExecute(@RequestPart String language, @RequestPart MultipartFile file) throws IOException {
         System.out.println(file.getContentType());
-        System.out.println(file.getName());
+        System.out.println(file.getOriginalFilename());
+        System.out.println(new String(file.getBytes()));
         return ResponseEntity.ok("Ok");
     }
 }
