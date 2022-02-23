ade1c388696d0e3d7f432d408458e53f4cce5308
Kenyo-cell <rekurt3000@gmail.com>
2022-02-23 20:39:45 +0300

    init: add gitignore and main controller

---
+++ src/main/java/ru/mirea/controller/MainController.java
@@ -0,0 +1,17 @@
+package ru.mirea.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class MainController {
+    @PostMapping("/")
+    public ResponseEntity<String> sendToExecute(@RequestPart MultipartFile file) {
+        System.out.println(file.getContentType());
+        System.out.println(file.getName());
+        return ResponseEntity.ok("Ok");
+    }
+}
