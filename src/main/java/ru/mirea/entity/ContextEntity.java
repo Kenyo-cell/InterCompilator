f84d94fd4cd80b74282e1cb6ba1b7f8d9005bea8
Kenyo-cell <rekurt3000@gmail.com>
2022-05-10 15:43:00 +0300

    feat: init entities

--- src/main/java/ru/mirea/entity/ContextEntity.java
+++ src/main/java/ru/mirea/entity/ContextEntity.java
@@ -1,4 +1,13 @@
 package ru.mirea.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ContextEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
