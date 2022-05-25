e8303a58c2aa84eed94bbcaec7df9a1f37f871f3
Kenyo-cell <rekurt3000@gmail.com>
2022-05-25 16:16:03 +0300

    feat: added host support

--- src/main/java/ru/mirea/service/container/ContainerDefinitionObject.java
+++ src/main/java/ru/mirea/service/container/ContainerDefinitionObject.java
@@ -3,12 +3,13 @@ package ru.mirea.service.container;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record ContainerDefinitionObject(String language, int port, boolean free) {
+public record ContainerDefinitionObject(String language, String host, int port, boolean free) {
     @JsonCreator
     ContainerDefinitionObject(
             @JsonProperty("language") String language,
+            @JsonProperty("host") String host,
             @JsonProperty("port") int port) {
 
-        this(language, port, true);
+        this(language, host, port, true);
     }
 }
